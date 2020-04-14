70b7d2edb4ecf09eac944c167e3f07c5828f752e
=Mithun <mithunadhikari40@gmail.com>
2020-04-14 09:13:47 +0545

    notification added

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_provider_arch/core/base_widget.dart';
 import 'package:flutter_provider_arch/core/constants/app_contstants.dart';
 import 'package:flutter_provider_arch/core/models/places.dart';
@@ -7,6 +9,10 @@ import 'package:flutter_provider_arch/viewmodels/home_view_model.dart';
 import 'package:provider/provider.dart';
 
 class HomeView extends StatelessWidget {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+   final  FirebaseMessaging  _firebaseMessaging = FirebaseMessaging();
+
   @override
   Widget build(BuildContext context) {
     return BaseWidget<HomeViewViewModel>(
@@ -50,7 +56,59 @@ class HomeView extends StatelessWidget {
     );
   }
 
-  _onModelReady(HomeViewViewModel model) {
+  _onModelReady(HomeViewViewModel model) async {
+
+
+
+
+    ///notification initialization --
+// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS = IOSInitializationSettings(
+        requestSoundPermission: true,
+        requestBadgePermission: true,
+        requestAlertPermission: true,
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: selectNotification);
+
+
+
+
+_firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        
+      },
+       onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+
+        
+      },
+    );
+
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(
+            sound: true, badge: true, alert: true, provisional: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      
+     
+      print("Firebase token $token");
+    });
+
+
+
     model.getAllPlaces();
     //initial data fetching
   }
@@ -59,9 +117,23 @@ class HomeView extends StatelessWidget {
       BuildContext context, HomeViewViewModel model) {
     return FloatingActionButton(
       child: Icon(Icons.add),
-      onPressed: () {
-        Navigator.of(context)
-            .pushNamed(RoutePaths.AddNewPlace, arguments: model);
+      onPressed: () async {
+        var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+          'your channel id',
+          'your channel name',
+          'your channel description',
+          importance: Importance.Max,
+          priority: Priority.High,
+        );
+        var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+        var platformChannelSpecifics = NotificationDetails(
+            androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+        await flutterLocalNotificationsPlugin.show(0,'Notification title',
+            'Notification body', platformChannelSpecifics,
+            payload: 'item x');
+
+        // Navigator.of(context)
+        //     .pushNamed(RoutePaths.AddNewPlace, arguments: model);
       },
     );
   }
@@ -88,4 +160,13 @@ class HomeView extends StatelessWidget {
 
   void onUpdate(Place place, HomeViewViewModel model) {}
   void onDelete(Place place, HomeViewViewModel model) {}
+
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+        
+      }
+
+  Future selectNotification(String payload) async {
+    print("selcet notification payload $payload");
+  }
 }
